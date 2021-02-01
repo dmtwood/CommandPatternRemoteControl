c253c8372c73626411ba07c4e84a55b8fb729d01
dmtwood <49392867+dmtwood@users.noreply.github.com>
2021-02-01 12:30:20 +0100

    fixed Garagedoor

---
+++ src/ehb/dt/command/remote/GarageDoorUpCommand.java
@@ -0,0 +1,12 @@
+package ehb.dt.command.remote;
+
+public class GarageDoorUpCommand implements Command {
+	GarageDoor garageDoor;
+	public GarageDoorUpCommand(GarageDoor garageDoor) {
+		this.garageDoor = garageDoor;
+	}
+	public void execute() {
+		garageDoor.up();
+		garageDoor.lightOn();
+	}
+}
